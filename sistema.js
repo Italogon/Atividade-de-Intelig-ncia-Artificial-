41ee0310ee3a4899d0d8ca795c86aa527ffa82a1
João Germano <testando123testando13@gmail.com>
2026-02-18 21:55:25 -0400

    Adiciona função recursiva auxiliar na DFS

--- sistema.js
+++ sistema.js
@@ -3,3 +3,9 @@ function dfs(grafo, inicio, destino) {
 function dfs(grafo, inicio, destino) {
     const visitados = new Set();
 }
+function dfs(grafo, inicio, destino) {
+    const visitados = new Set();
+
+    function buscar(no, caminho) {
+    }
+}
