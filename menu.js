446bf99188b8760c4d1d7902aafbf5f258516e0e
MR-Siebert <matheushfayas@gmail.com>
2026-02-20 01:33:01 -0300

    feat(menu): adiciona criação do grafo e arestas iniciais

--- menu.js
+++ menu.js
@@ -1,39 +1,7 @@
-const bfs = require('./bfs');
-const dfs = require('./dfs');
-const readline = require('readline');
+const grafo = new Grafo();
 
-const grafo = {
-    A: ['B', 'C'],
-    B: ['D'],
-    C: ['D'],
-    D: []
-};
+grafo.adicionarAresta('A', 'B');
+grafo.adicionarAresta('A', 'C');
+grafo.adicionarAresta('B', 'D');
+grafo.adicionarAresta('C', 'D');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-function mostrarMenu() {
-    console.log("\n=== MENU ===");
-    console.log("1 - Executar BFS");
-    console.log("2 - Executar DFS");
-    console.log("3 - Sair");
-
-    rl.question("Escolha uma opção: ", (opcao) => {
-        if (opcao === "1") {
-            console.log("Resultado BFS:", bfs(grafo, 'A', 'D'));
-            mostrarMenu();
-        } else if (opcao === "2") {
-            console.log("Resultado DFS:", dfs(grafo, 'A', 'D'));
-            mostrarMenu();
-        } else if (opcao === "3") {
-            rl.close();
-        } else {
-            console.log("Opção inválida!");
-            mostrarMenu();
-        }
-    });
-}
-
-mostrarMenu();
