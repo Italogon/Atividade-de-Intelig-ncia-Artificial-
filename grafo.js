d38ceb4a263ca635cb5de26d8bf0f78ca94aa584
MR-Siebert <matheushfayas@gmail.com>
2026-02-20 02:02:38 -0300

    feat(grafo): adiciona método adicionarNo

--- grafo.js
+++ grafo.js
@@ -2,6 +2,12 @@ class Grafo {
     constructor() {
         this.listaAdj = {};
     }
+
+    adicionarNo(no) {
+        if (!this.listaAdj[no]) {
+            this.listaAdj[no] = [];
+        }
+    }
 }
 
 module.exports = Grafo;
