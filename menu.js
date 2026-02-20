ab0a744651dd4bc9e7f01a27da0385a7a2915eac
MR-Siebert <matheushfayas@gmail.com>
2026-02-20 01:41:05 -0300

    feat(menu): integra chamadas de BFS e DFS no menu principal

--- menu.js
+++ menu.js
@@ -1,4 +1,3 @@
-function executarDFS() {
-    const resultado = dfs(grafo, 'A', 'D');
-    console.log("Resultado DFS:", resultado);
-}
+console.log("=== MENU DE TESTE ===");
+executarBFS();
+executarDFS();
