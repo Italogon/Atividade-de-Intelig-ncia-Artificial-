ee4ab6aa481a825b7b24e15858367a7c0b1baabe
João Germano <testando123testando13@gmail.com>
2026-02-22 14:22:59 -0400

    Implementa lógica de busca em profundidade e verificação de destino

--- dfs.js
+++ dfs.js
@@ -2,7 +2,20 @@ function dfs(grafo, inicio, destino) {
     const visitados = new Set();
 
     function buscar(no, caminho) {
-        
+        if (no === destino) {
+            return caminho;
+        }
+
+        visitados.add(no);
+
+        for (let vizinho of grafo.listaAdj[no]) {
+            if (!visitados.has(vizinho)) {
+                const resultado = buscar(vizinho, [...caminho, vizinho]);
+                if (resultado) return resultado;
+            }
+        }
+
+        return null;
     }
 }
 
